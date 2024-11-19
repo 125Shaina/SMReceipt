7a4531bb79b701e70e5c8d0befbf55c734c3b576
Shaina Mitra <125smitra@frhsd.com>
2024-11-19 14:05:14 -0500

    added stuff

--- receipt/src/App.tsx
+++ receipt/src/App.tsx
@@ -3,6 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 import jsonData from "./receipt1.json"
 
+export interface food{
+    type:string;
+    price:number;
+}
+interface cookie extends food{
+    quantity:number;
+    variety:string;
+}
+interface cake extends food{
+    shape: string;
+    radius?: number;
+    message: string;
+    flavor: string;
+    length?: number;
+    width?:number;
+}
+interface doughnut extends food{
+    flavor:string;
+    quantity:number;
+}
 function App()
 {
     var receipt = jsonData.receipt;
--- receipt/src/receipt.tsx
+++ receipt/src/receipt.tsx
@@ -1,27 +1,13 @@
-interface food{
-    type:string;
-    price:number;
-}
-interface cookie extends food{
-    quantity:number;
-    variety:string;
-}
-interface cake extends food{
-    shape: string;
-    radius?: number;
-    message: string;
-    flavor: string;
-    length?: number;
-    width?:number;
-}
-interface doughnut extends food{
-    flavor:string;
-    quantity:number;
-}
+import {food} from"./App"
+
+
 export default function receipt(receipt: food[])
 {
 
     return (
-     receipt.map(item => <Item item = {item}/>);
+        <>
+            receipt.map(item => <Item item = {item}/>);
+
+        </>
     );
 }
