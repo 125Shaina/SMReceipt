26d7e69db5429680d05da5e94c5721564741a54c
Shaina Mitra <125smitra@frhsd.com>
2024-11-22 12:50:55 -0500

    added pics and items

--- receipt/src/App.tsx
+++ receipt/src/App.tsx
@@ -2,16 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import jsonData from "./receipt1.json"
+import Receipt from "./receipt";
 
 export interface food{
     type:string;
     price:number;
 }
-interface cookie extends food{
+export interface cookie extends food{
     quantity:number;
     variety:string;
 }
-interface cake extends food{
+export interface cake extends food{
     shape: string;
     radius?: number;
     message: string;
@@ -19,7 +20,7 @@ interface cake extends food{
     length?: number;
     width?:number;
 }
-interface doughnut extends food{
+export interface doughnut extends food{
     flavor:string;
     quantity:number;
 }
@@ -28,7 +29,8 @@ function App()
     var receipt = jsonData.receipt;
   return (
     <div className="Receipt">
-     <h1> cookie</h1>
+        <h1> Receipt</h1>
+     <Receipt receipt={receipt} />
     </div>
   );
 }
